25c68986807a3f41388678f36b45df3d1c647d90
kiptr <2011016210014@mhs.ulm.ac.id>
2025-01-28 20:39:57 +0800

    Add death logic, player swap and timer

--- src/main/java/com/loki/deathswap/DeathSwapPlugin.java
+++ src/main/java/com/loki/deathswap/DeathSwapPlugin.java
@@ -3,9 +3,11 @@ package com.loki.deathswap;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -37,6 +39,46 @@ public class DeathSwapPlugin extends JavaPlugin {
         Bukkit.broadcastMessage("The game is over! " + winnerName + " wins!");
         players.clear();
         gameRunning = false;
+    }
+
+    public void startDeathSwap() {
+        new BukkitRunnable() {
+            int countdown = 10;
+
+            @Override
+            public void run() {
+                if (countdown > 0) {
+                    Bukkit.broadcastMessage(ChatColor.GOLD + "Death Swap will start in " + countdown + " seconds.");
+                    countdown--;
+                } else {
+                    swapPlayers();
+                    countdown = 10;
+                }
+            }
+        }.runTaskTimer(this, 0L, 20L);
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                swapPlayers();
+            }
+        }.runTaskTimer(this, 18000L, 18000L);
+    }
+
+    public void swapPlayers() {
+        if (players.size() != 2) {
+            return;
+        }
+
+        Player player1 = players.get(0);
+        Player player2 = players.get(1);
+        Location location1 = player1.getLocation();
+        Location location2 = player2.getLocation();
+
+        player1.teleport(location2);
+        player2.teleport(location1);
+
+        Bukkit.broadcastMessage(ChatColor.GREEN + "Players have swapped locations!");
     }
 
     @Override
